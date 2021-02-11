714fdf9df27938d9866ab816d45562d6877b1eca
Tomasz Flis <tomaszflis@MacBook-Pro-Tomasz.local>
2021-02-11 21:24:44 +0100

    build(project): init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TuiRootModule } from '@taiga-ui/core';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      TopBarComponent
+   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TuiRootModule
   ],
   providers: [],
   bootstrap: [AppComponent]
